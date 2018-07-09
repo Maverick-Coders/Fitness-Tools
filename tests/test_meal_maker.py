efdf2a11887ad56a362e0993eb33d7ebe5c74792
Jeff <dshostakovich30@gmail.com>
2018-07-09 13:30:21 -0500

    Add tests for assigned macronutrient percentages by body type.

--- tests/test_meal_maker.py
+++ tests/test_meal_maker.py
@@ -4,4 +4,31 @@ import pytest
 
 class TestMealMaker(object):
 
-    pass
+    # Test assigned macronutrient percentages by body type
+
+    @pytest.mark.parametrize("body_type, expected",
+                            [
+                            ('MESOMORPH', 0.3),
+                            ('Ectomorph', 0.2), 
+                            ('endomorph', 0.4),
+                            ])
+    def test_fat_percent(self, body_type, expected):
+        assert meal_maker.MakeMeal(150, body_type=body_type).fat_percent == expected
+
+    @pytest.mark.parametrize("body_type, expected",
+                            [
+                            ('MESOMORPH', 0.3),
+                            ('Ectomorph', 0.25), 
+                            ('endomorph', 0.35),
+                            ])
+    def test_protein_percent(self, body_type, expected):
+        assert meal_maker.MakeMeal(150, body_type=body_type).protein_percent == expected
+
+    @pytest.mark.parametrize("body_type, expected",
+                            [
+                            ('MESOMORPH', 0.4),
+                            ('Ectomorph', 0.55), 
+                            ('endomorph', 0.25),
+                            ])
+    def test_carb_percent(self, body_type, expected):
+        assert meal_maker.MakeMeal(150, body_type=body_type).carb_percent == expected
