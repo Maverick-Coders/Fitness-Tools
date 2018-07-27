4ceea270e625d43bbed17a7c9bd4c8f28cff055d
synergetic wellness <dshostakovich30@gmail.com>
2018-07-27 20:32:58 +0000

    Merged PR 5: Release 0.1.0
    - restructured module structure fitness_tools top level package.
    - Correct examples to reflect fitness_tools as top level package.  Fix spelling error in exercise_details.
    - correct spelling in exercise_details.
    - Add setup.py
    - Amend changelog date.
    
    Related work items: #76, #84

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'fitness-tools',
+    packages = setuptools.find_packages(),
+    version = '0.1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Healthy Lifestyles With Python',
+    long_description = long_description,
+    author = 'Maverick Coders',
+    author_email = 'maverickcoders@pm.me',
+    url = 'https://github.com/Maverick-Coders/Fitness-Tools',
+    keywords = ['health', 'wellness', 'fitness', 'exercise', 'nutrition'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ),
+)
